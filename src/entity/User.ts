3500f086d94c38021dd238ae5eb29c5ebc3f0949
NNeast <zawislaktomasz4@gmail.com>
2018-06-26 20:23:38 +0200

    add User class

---
+++ src/entity/User.ts
@@ -0,0 +1,19 @@
+import { prop, arrayProp } from 'typegoose'
+import Series from './Series'
+
+export default class User {
+    @prop({ unique: true })
+    id: string
+
+    @prop({unique: true})
+    username: string
+
+    @prop({unique: true})
+    email: string
+
+    @prop()
+    password: string
+
+    @arrayProp({ items: Series })
+    series: Series[]
+}
