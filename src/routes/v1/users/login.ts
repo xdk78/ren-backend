d13e9d33a1f8b22ae52d24ced7994319351540fb
NNeast <zawislaktomasz4@gmail.com>
2018-06-29 22:05:44 +0200

    add password hashing

--- src/routes/v1/users/login.ts
+++ src/routes/v1/users/login.ts
@@ -6,6 +6,7 @@ import * as bcrypt from 'bcrypt'
 export default async (fastify, opts, next) => {
   const db = fastify.mongo.db
   const saltRounds = 10
+
   fastify.post('/users/login', async (request: FastifyRequest<ServerRequest>, reply: FastifyReply<ServerResponse>) => {
     try {
       reply.header('Content-Type', 'application/json').code(200)
