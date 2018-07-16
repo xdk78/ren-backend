2a20ac2828ece987fac6820725ff3971bbe9c8e6
xdk78 <xdk78@interia.pl>
2018-07-16 21:30:00 +0200

    Add more modfiy methods for season and series

--- src/entity/series/Season.ts
+++ src/entity/series/Season.ts
@@ -1,10 +1,21 @@
-import { prop, arrayProp, Typegoose } from 'typegoose'
+import { prop, arrayProp, Typegoose, Ref, staticMethod, ModelType } from 'typegoose'
 import Episode from './Episode'
 
 export default class Season extends Typegoose {
   @prop()
   number: number
 
-  @arrayProp({ items: Episode })
-  episodes: Episode[]
+  @arrayProp({ itemsRef: Episode })
+  episodes: Ref<Episode>[]
+
+  @staticMethod
+  static addEpisode(this: ModelType<Season> & typeof Season, seasonId: Ref<Season>, episodeId: Ref<Season>) {
+    return this.updateOne({ _id: seasonId }, { $push: { episodes: episodeId } })
+  }
+
+  @staticMethod
+  static removeEpisode(this: ModelType<Season> & typeof Season, seasonId: Ref<Season>, episodeId: Ref<Season>) {
+    return this.updateOne({ _id: seasonId }, { $pull: { episodes: episodeId } })
+  }
+
 }
--- src/entity/series/Series.ts
+++ src/entity/series/Series.ts
@@ -1,4 +1,4 @@
-import { prop, Typegoose, arrayProp, staticMethod, ModelType, Ref } from 'typegoose'
+import { prop, Typegoose, arrayProp, staticMethod, ModelType, Ref, instanceMethod } from 'typegoose'
 import Genre from './Genre'
 import Season from './Season'
 import Category from './Category'
@@ -29,12 +29,42 @@ export default class Series extends Typegoose {
 
   @staticMethod
   static addSeason(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, seasonId: Ref<Season>) {
-    return this.updateOne({ _id: seriesId }, { $push: { seasons: seasonId } })
+    return this.update({ _id: seriesId }, { $push: { seasons: seasonId } })
   }
 
   @staticMethod
   static removeSeason(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, seasonId: Ref<Season>) {
-    return this.updateOne({ _id: seriesId }, { $pull: { seasons: seasonId } })
+    return this.update({ _id: seriesId }, { $pull: { seasons: seasonId } })
+  }
+
+  @staticMethod
+  static setCategory(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, categoryId: Ref<Category>) {
+    return this.updateOne({ _id: seriesId }, { $set: { category: categoryId } })
+  }
+
+  @staticMethod
+  static removeCategory(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, categoryId: Ref<Category>) {
+    return this.updateOne({ _id: seriesId }, { $unset: { category: categoryId } })
+  }
+
+  @staticMethod
+  static addGenre(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, genreId: Ref<Genre>) {
+    return this.update({ _id: seriesId }, { $push: { genres: genreId } })
+  }
+
+  @staticMethod
+  static removeGenre(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, genreId: Ref<Genre>) {
+    return this.update({ _id: seriesId }, { $pull: { genres: genreId } })
+  }
+
+  @staticMethod
+  static setRating(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, rating: number) {
+    return this.updateOne({ _id: seriesId }, { $set: { rating: rating } })
+  }
+
+  @staticMethod
+  static incrementRating(this: ModelType<Series> & typeof Series, seriesId: Ref<Series>, incrementSize: number) {
+    return this.updateOne({ _id: seriesId }, { $inc: { rating: incrementSize } })
   }
 
 }
