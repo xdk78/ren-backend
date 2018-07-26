f43f129de3970bd08045a0d10dbba344674f4f52
Rekseto <ReksetoAjx@gmail.com>
2018-07-26 15:10:44 +0200

    Better segregation of bugs propagated in middleware

--- src/routes/v1/middlewares/isAuthorized.ts
+++ src/routes/v1/middlewares/isAuthorized.ts
@@ -7,10 +7,10 @@ const defaultMethods = ['GET', 'PUT', 'DELETE', 'POST', 'PATCH']
 
 export default (connection, methods = defaultMethods) => async function (this: FastifyInstance, req: IncomingMessage, res: ServerResponse, next) {
   if (methods.includes(req.method)) {
-    const token = extractToken(req)
-    const payload = decodeToken(token) as any
     const userModel = new User().getModelForClass(User, { existingConnection: connection })
     try {
+      const token = extractToken(req)
+      const payload = decodeToken(token) as any
       const user = await userModel.findById(payload.payload.id)
       const secret = `${user.secret}@${String(process.env.API_JWT_SECRET_TOKEN)}`
       const data = await verifyToken(token, secret)
