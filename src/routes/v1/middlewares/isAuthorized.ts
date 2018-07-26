53926ca8de35d5b4931d8a97dfbb9100b9f6ca07
Rekseto <ReksetoAjx@gmail.com>
2018-07-26 02:28:36 +0200

    Added specific methods for middleware

--- src/routes/v1/middlewares/isAuthorized.ts
+++ src/routes/v1/middlewares/isAuthorized.ts
@@ -5,30 +5,38 @@ import { extractToken, decodeToken, verifyToken } from '../../../utils/authUtils
 import User from '../../../entity/User'
 import { scrypt } from 'crypto'
 
-export default (connection) => async function (request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>, next: any) {
-  const token = extractToken(request)
-  const payload = decodeToken(token)
-  const userModel = new User().getModelForClass(User, { existingConnection: connection })
-    // @ts-ignore
-  const user = await userModel.findById(payload.payload.id)
-    // @ts-ignore
-  const secret = `${user.secret}@${String(process.env.API_JWT_SECRET_TOKEN)}`
-  try {
-    const data = await verifyToken(token, secret)
-    // @ts-ignore
-    request.root = user
-    // @ts-ignore
-    request.token = data
+const defaultMethods = ['GET', 'PUT', 'DELETE', 'POST', 'PATCH']
 
-    return next()
-  } catch (error) {
-    // @ts-ignore
-    reply.setHeader('content-type', 'application/json')
-    // @ts-ignore
-    reply.statusCode = 401
-    // @ts-ignore
-    reply.end(JSON.stringify({
-      error: 'Unauthorized',
-    }))
+export default (connection, methods = defaultMethods) => async function (request: FastifyRequest<IncomingMessage>, reply: FastifyReply<ServerResponse>, next: any) {
+  // @ts-ignore
+  if (methods.includes(request.method)) {
+    const token = extractToken(request)
+    const payload = decodeToken(token)
+    const userModel = new User().getModelForClass(User, { existingConnection: connection })
+    try {
+      // @ts-ignore
+      const user = await userModel.findById(payload.payload.id)
+      // @ts-ignore
+      const secret = `${user.secret}@${String(process.env.API_JWT_SECRET_TOKEN)}`
+      const data = await verifyToken(token, secret)
+      // @ts-ignore
+      request.root = user
+      // @ts-ignore
+      request.token = data
+
+      return next()
+    } catch (error) {
+      // @ts-ignore
+      reply.setHeader('content-type', 'application/json')
+      // @ts-ignore
+      reply.statusCode = 401
+      // @ts-ignore
+      reply.end(JSON.stringify({
+        error: 'Unauthorized',
+      }))
+    }
   }
+
+  return next()
+
 }
